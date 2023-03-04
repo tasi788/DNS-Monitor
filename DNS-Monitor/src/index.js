606a579f8116ea0ff4200bf864391cf511bce201
踢低吸 <tdc@tdccc.com.tw>
2023-03-04 18:14:01 +0000

    ✨ works

--- DNS-Monitor/src/index.js
+++ DNS-Monitor/src/index.js
@@ -1,10 +1,38 @@
-function rawHtmlResponse(html) {
-  const init = {
+async function changeDNS(bool) {
+  // makes requests 
+
+  // first to get record id
+  const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records`
+  let init = {
+    method: 'GET',
     headers: {
-      'content-type': 'text/html;charset=UTF-8',
+      'Authorization': `Bearer ${cf_token}`
+    }
+  }
+  let resp_ = await fetch(url + `?name=${dns_name}`, init)
+  let resp = await resp_.json()
+  let record_id = resp.result[0].id
+
+  // if bool === true die
+  // else === to home
+  let body = {
+    "type": "CNAME",
+    "name": `${dns_name}`,
+    "content": bool === true ? `${remote_server}` : `${home_lab}`,
+    "ttl": 60,
+    "proxied": true,
+  }
+  init = {
+    body: JSON.stringify(body),
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+      'Authorization': `Bearer ${cf_token}`
     },
   };
-  return new Response(html, init);
+  console.log(init);
+  let r = await fetch(url + `/${record_id}`, init);
+  console.log(await r.json());
 }
 
 /**
@@ -14,14 +42,21 @@ function rawHtmlResponse(html) {
  */
 async function readRequestBody(request) {
   // request.body.json()
-  const { headers } = request;
+  const {
+    headers
+  } = request;
   const contentType = headers.get('content-type') || '';
 
   if (contentType.includes('application/json')) {
     let data = await request.json();
+
     if (data.heartbeat != true) {
       // do something
-      return 'good';
+      await changeDNS(true);
+      return 'set to remote server'
+    } else if (data.heartbeat === true) {
+        await changeDNS(false)
+        return 'set to home server';
     }
   } else {
     // Perhaps some other type of data was submitted in the form
@@ -32,16 +67,16 @@ async function readRequestBody(request) {
 
 async function handleRequest(request) {
   const reqBody = await readRequestBody(request);
-  const retBody = `The request body sent in was ${reqBody}`;
-  return new Response(retBody);
+  return new Response(reqBody);
 }
 
 addEventListener('fetch', event => {
-  const { request } = event;
+  const {
+    request
+  } = event;
   if (request.method === 'POST') {
     return event.respondWith(handleRequest(request));
   } else if (request.method === 'GET') {
     return event.respondWith(new Response(`The request was a GET`));
   }
 });
-
